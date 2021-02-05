cf8d199d9706074b1f57ce11fb174a8a5ed5fd16
azukiazusa1 <cycleippei@gmail.com>
2021-02-05 15:16:17 +0900

    cursorIndex

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -86,9 +86,16 @@ export class PostResolver {
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit)
     const realLimitPlusOne = realLimit + 1
-    const replacements: any[] = [realLimitPlusOne, req.session.userId]
+    const replacements: any[] = [realLimitPlusOne]
+
+    if (req.session.userId) {
+      replacements.push(req.session.userId)
+    }
+
+    let cursorIndex = 3
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)))
+      cursorIndex = replacements.length
     }
 
     const posts = await getConnection().query(
@@ -108,7 +115,7 @@ export class PostResolver {
       }
       from post p
       inner join public.user u on u.id = p."creatorId"
-      ${cursor ? `where p."createdAt" < $3` : ''}
+      ${cursor ? `where p."createdAt" < $${cursorIndex}` : ''}
       order by p."createdAt" DESC
       limit $1
       `,
